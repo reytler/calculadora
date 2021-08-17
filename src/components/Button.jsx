923c054b23e589d0ae466792dcd17bf6afbcf2a1
reytler <reytlercompare@hotmail.com>
2021-08-17 10:34:22 -0300

    Correção bug tamanho botão

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import './Button.css'
 
 export default function Button(props){
-        let classes = 'button'
+        let classes = 'button '
         classes += props.operation ? 'operation' : ''
         classes += props.double ? 'double' : ''
         classes += props.triple ? 'triple' : ''
-
     return(
         <button 
             onClick={e => props.click && props.click(props.label)}
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -44,7 +44,7 @@ export default class Calculator extends Component {
                 <Button label="2" click={this.addDigit} />
                 <Button label="3" click={this.addDigit} />
                 <Button label="+" click={this.setOperation} operation/>
-                <Button label="0" click={this.addDigit} />
+                <Button label="0" click={this.addDigit} double/>
                 <Button label="." click={this.addDigit} />
                 <Button label="=" click={this.setOperation} operation/>
             </div>
